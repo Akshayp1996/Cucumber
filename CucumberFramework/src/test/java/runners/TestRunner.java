3b73483568c7222f6c76c6197c93d78e45abc1b8
Akshaypatil <patilakshay2323@gmail.com>
2023-12-07 11:34:24 +0530

    Through eclipse

--- CucumberFramework/src/test/java/runners/TestRunner.java
+++ CucumberFramework/src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src//test//resources//functionalTests", glue = {
+@CucumberOptions(features = "src/test/resources/functionalTests", glue = {
 		"stepDefinations" }, monochrome = false, dryRun = false, plugin = { "pretty", "html:target/cucumber.html",
 				"json:target/cucumber.json", "junit:target/cucumberReport.xml", })
 public class TestRunner {
