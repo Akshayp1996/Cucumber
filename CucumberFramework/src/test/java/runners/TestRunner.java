4d5822c13cdca04b4888a6b3a37037462bee2411
Akshaypatil <patilakshay2323@gmail.com>
2023-12-13 15:58:11 +0530

    Runner file has only single tag

--- CucumberFramework/src/test/java/runners/TestRunner.java
+++ CucumberFramework/src/test/java/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/functionalTests", glue = {
-		"stepDefinations" }, tags = "@Test1 , @Test2", monochrome = false, dryRun = false, plugin = { "pretty",
+		"stepDefinations" }, tags = "@Test1", monochrome = false, dryRun = false, plugin = { "pretty",
 				"html:target/cucumber.html", "json:target/cucumber.json", "junit:target/cucumberReport.xml", })
 public class TestRunner {
 }
